dddc6e97d238c6eb82108313161f70f4cc50f43b
Ahmad Musa <mycontribusion01@gmail.com>
2025-08-11 13:37:17 +0100

    / removed from url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://poetrychat.netlify.app/', // Allow requests from your React frontend https://poetrychat.netlify.app http://localhost:3000
+  origin: 'https://poetrychat.netlify.app', // Allow requests from your React frontend https://poetrychat.netlify.app http://localhost:3000
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization', 'X-Requested-With'],
   credentials: true
